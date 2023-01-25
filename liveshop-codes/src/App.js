d07cdb7741dcf3265a8f9ef3fda6d88dcf7db45e
Abdulloh Ibrohimov <109219970+ibrohimov0@users.noreply.github.com>
2023-01-25 21:55:51 +0500

    fully edited

--- liveshop-codes/src/App.js
+++ liveshop-codes/src/App.js
@@ -7,7 +7,7 @@ import NdCard from './common/card/ndCard';
 function App() {
   return (
     <div className="App">
-      <NdCard title="Acer A210" img={img} text="lorem ipsum dolor sit amet koret naret boret varet nice sdfs sdfe asde asfetf bruh wad nvsd sdf ew awe few  fewf trefge hrhu hhiubih iuhiuh" oldCost="$1.300" cost="$1.000" sale="$1.000"/>
+      <StCard title="Acer A210" img={img} text="lorem ipsum dolor sit amet koret naret boret varet nice sdfs sdfe asde asfetf bruh wad nvsd sdf ew awe few  fewf trefge hrhu hhiubih iuhiuh" oldCost="$1.300" cost="$1.000" sale="10%"/>
     </div>
   );
 }
